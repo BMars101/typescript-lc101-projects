90b10bb412c2bd0ca9e110cc94040dd5bac03a89
Beth Marshall <beth.ellieday@gmail.com>
2022-06-17 10:28:30 -0400

    TypeScript studio exercises

---
+++ studio/Rocket.js
@@ -0,0 +1,51 @@
+"use strict";
+exports.__esModule = true;
+exports.Rocket = void 0;
+var Rocket = /** @class */ (function () {
+    function Rocket(name, totalCapacityKg) {
+        this.totalCapacityKg = totalCapacityKg;
+        this.name = name;
+    }
+    Rocket.prototype.sumMass = function (items) {
+        var sum = 0;
+        items.forEach(function (item) {
+            sum += item.massKg;
+        });
+        // for(let i = 0; i < items.length; i++){
+        //     sum += items[i].massKg;
+        // }
+        return sum;
+    };
+    Rocket.prototype.currentMassKg = function () {
+        var combinedMass = this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+        return combinedMass;
+    };
+    Rocket.prototype.canAdd = function (item) {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addCargo = function (cargo) {
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addAstronaut = function (astronaut) {
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    return Rocket;
+}());
+exports.Rocket = Rocket;
